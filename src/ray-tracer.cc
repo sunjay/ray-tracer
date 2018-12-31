23ff7be6caf304c36c8e3452744f4ae566eb177d
Sunjay Varma <varma.sunjay@gmail.com>
2018-12-31 15:09:50 -0700

    Hitting a sphere with the ray

--- src/ray-tracer.cc
+++ src/ray-tracer.cc
@@ -6,10 +6,35 @@
 
 using namespace std;
 
+bool hit_sphere(const vec3 &center, double radius, const ray &r) {
+    // Equation for sphere: x*x + y*y + z*z = R*R
+    // With arbitrary center: (x-cx)*(x-cx) + (y-cy)*(y-cy) + (z-cz)*(z-cz) = R*R
+    // In vector form: (p - c).dot(p - c) = R*R where p = (x, y, z), c = (cx, cy, cz)
+    // To check if ray intersects sphere, let p = r where r = a + t*b
+    // This gives us: (r - c).dot(r - c) = R*R
+    // Substituting: (a + t*b - c).dot(a + t*b - c) = R*R
+    // Expanding: t*t*b.dot(b) + 2*t*b.dot(a-c) + (a-c).dot(a-c) - R*R = 0
+    // From the quadratic formula, we get the discriminant b*b - 4*a*c
+    // This tells us the number of solutions and we can interpret it as the
+    // number of intersections. As long as the discriminant tells us there are
+    // at least two intersections, the ray passes through the sphere. One
+    // intersection means that the ray just hits on the edge.
+    vec3 oc = r.origin() - center;
+    double a = r.direction().dot(r.direction());
+    double b = 2.0 * oc.dot(r.direction());
+    double c = oc.dot(oc) - radius * radius;
+    double discriminant = b*b - 4*a*c;
+    return discriminant > 0;
+}
+
 // Computes the color seen in the direction of the ray.
 // Computes what this ray intersects with and the color of that intersection point.
 vec3 color(const ray &r) {
-    // Just return the background for now
+    if (hit_sphere(vec3(0, 0, -1), 0.5, r)) {
+        return vec3(0, 1, 1);
+    }
+
+    // If nothing else is hit, return the background
 
     // Make into unit vector so -1.0 < y < 1.0
     vec3 unit_direction = r.direction().to_unit();
