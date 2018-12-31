846dc3f1c101da083a68a9ccc238e66a526289fa
Sunjay Varma <varma.sunjay@gmail.com>
2018-12-31 13:39:18 -0700

    Outputting test PPM file

--- src/ray-tracer.cc
+++ src/ray-tracer.cc
@@ -1,8 +1,21 @@
 #include <iostream>
+
+#include "vec3.hpp"
+#include "ppm.hpp"
+
 using namespace std;
 
-int main() 
-{
-    cout << "Hello, World!";
+int main() {
+    int nx = 200;
+    int ny = 100;
+    ppm_image img(cout, nx, ny);
+    for (int j = ny-1; j >= 0; j--) {
+        for (int i = 0; i < nx; i++) {
+            vec3 col(float(i) / float(nx), float(j) / float(ny), 0.2);
+            img.add(col);
+        }
+    }
+    img.finish();
+
     return 0;
 }
