b302b188447c43e64d10851c342761407568fbb0
Sunjay Varma <varma.sunjay@gmail.com>
2018-12-31 00:32:46 -0700

    Base ray-tracer project with vec3 class

---
+++ src/vec3.cc
@@ -0,0 +1,117 @@
+#include <cmath>
+
+#include "vec3.hpp"
+
+vec3::vec3() {}
+vec3::vec3(double e0, double e1, double e2) {
+    e[0] = e0;
+    e[1] = e1;
+    e[2] = e2;
+}
+
+double vec3::x() const {
+    return e[0];
+}
+double vec3::y() const {
+    return e[1];
+}
+double vec3::z() const {
+    return e[2];
+}
+
+double vec3::r() const {
+    return e[0];
+}
+double vec3::g() const {
+    return e[1];
+}
+double vec3::b() const {
+    return e[2];
+}
+
+const vec3 &vec3::operator+() const {
+    return *this;
+}
+vec3 vec3::operator-() const {
+    return vec3(-e[0], -e[1], -e[2]);
+}
+
+double vec3::operator[](int i) const {
+    return e[i];
+}
+double &vec3::operator[](int i) {
+    return e[i];
+}
+
+vec3 &vec3::operator+=(const vec3 &other) {
+    e[0] += other.e[0];
+    e[1] += other.e[1];
+    e[2] += other.e[2];
+    return *this;
+}
+vec3 &vec3::operator-=(const vec3 &other) {
+    e[0] -= other.e[0];
+    e[1] -= other.e[1];
+    e[2] -= other.e[2];
+    return *this;
+}
+vec3 &vec3::operator*=(const vec3 &other) {
+    e[0] *= other.e[0];
+    e[1] *= other.e[1];
+    e[2] *= other.e[2];
+    return *this;
+}
+vec3 &vec3::operator/=(const vec3 &other) {
+    e[0] /= other.e[0];
+    e[1] /= other.e[1];
+    e[2] /= other.e[2];
+    return *this;
+}
+vec3 &vec3::operator*=(double t) {
+    e[0] *= t;
+    e[1] *= t;
+    e[2] *= t;
+    return *this;
+}
+vec3 &vec3::operator/=(double t) {
+    e[0] /= t;
+    e[1] /= t;
+    e[2] /= t;
+    return *this;
+}
+
+double vec3::length() const {
+    return sqrt(square_length());
+}
+double vec3::square_length() const {
+    return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
+}
+void vec3::make_unit_vector() {
+    *this /= length();
+}
+vec3 vec3::to_unit() const {
+    vec3 ret = *this;
+    ret /= length();
+    return ret;
+}
+
+double vec3::dot(const vec3 &other) const {
+    return e[0] * other.e[0] + e[1] * other.e[1] + e[2] * other.e[2];
+}
+
+vec3 vec3::cross(const vec3 &other) const {
+    return vec3(
+        e[1] * other.e[2] - e[2] * other.e[1],
+        -(e[0] * other.e[2] - e[2] * other.e[0]),
+        e[0] * other.e[1] - e[1] * other.e[0]
+    );
+}
+
+std::istream &operator>>(std::istream &is, vec3 &t) {
+    is >> t.e[0] >> t.e[1] >> t.e[2];
+    return is;
+}
+std::ostream &operator<<(std::ostream &os, const vec3 &t) {
+    os << t.e[0] << ' ' << t.e[1] << ' ' << t.e[2];
+    return os;
+}
---
+++ src/vec3.hpp
@@ -0,0 +1,47 @@
+#ifndef VEC3_HPP
+#define VEC3_HPP
+
+#include <iostream>
+
+// 3D X,Y,Z Vector and RGB Color class
+class vec3 {
+    double e[3];
+
+public:
+    vec3();
+    vec3(double e0, double e1, double e2);
+
+    double x() const;
+    double y() const;
+    double z() const;
+
+    double r() const;
+    double g() const;
+    double b() const;
+
+    const vec3 &operator+() const;
+    vec3 operator-() const;
+
+    double operator[](int i) const;
+    double &operator[](int i);
+
+    vec3 &operator+=(const vec3 &other);
+    vec3 &operator-=(const vec3 &other);
+    vec3 &operator*=(const vec3 &other);
+    vec3 &operator/=(const vec3 &other);
+    vec3 &operator*=(double t);
+    vec3 &operator/=(double t);
+
+    double length() const;
+    double square_length() const;
+    void make_unit_vector();
+    vec3 to_unit() const;
+
+    double dot(const vec3 &other) const;
+    vec3 cross(const vec3 &other) const;
+
+    friend std::istream &operator>>(std::istream &is, vec3 &t);
+    friend std::ostream &operator<<(std::ostream &os, const vec3 &t);
+};
+
+#endif /* end of include guard: VEC3_HPP */
