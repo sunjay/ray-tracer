6d48d5c3883c91d202057c3c49e3f2a05a640453
Sunjay Varma <varma.sunjay@gmail.com>
2018-12-31 14:01:27 -0700

    Added notes for how the color function works

--- src/ray-tracer.cc
+++ src/ray-tracer.cc
@@ -9,9 +9,14 @@ using namespace std;
 // Computes the color seen in the direction of the ray.
 // Computes what this ray intersects with and the color of that intersection point.
 vec3 color(const ray &r) {
-    vec3 unit_direction = r.direction().to_unit();
     // Just return the background for now
+
+    // Make into unit vector so -1.0 < y < 1.0
+    vec3 unit_direction = r.direction().to_unit();
+    // Scales the y value so 0.0 < t < 1.0
     float t = 0.5*(unit_direction.y() + 1.0);
+    // This is a linear interpolation (lerp) which always has the form:
+    // blended_value = (1 - t) * start_value + t*end_value with t = 0.0 to 1.0
     return (1.0 - t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
 }
 
